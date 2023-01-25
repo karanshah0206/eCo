88113c345da31d121170f62d543fae6af2dc2f58
Karan Shah <karanshah0206@outlook.com>
2023-01-25 19:56:14 +0530

    Specify Entity Framework Schema for Product and Generate Migrations

--- API/Program.cs
+++ API/Program.cs
@@ -1,9 +1,12 @@
+using API.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddDbContext<StoreContext>(x => x.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
