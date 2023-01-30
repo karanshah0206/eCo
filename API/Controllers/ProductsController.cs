2fcbb6f26c7733d403e23b0a63bd381bacf1fedf
Karan Shah <karanshah0206@outlook.com>
2023-01-30 13:52:21 +0530

    Implemented Product Repository Methods

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -1,30 +1,29 @@
-using Infrastructure.Data;
 using Core.Entities;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
+using Core.Interfaces;
 
-namespace APi.Controllers;
+namespace API.Controllers;
 
 [ApiController]
 [Route("[controller]")]
 public class ProductsController : ControllerBase
 {
-    private readonly StoreContext _context;
+    private readonly IProductRepository _repo;
 
-    public ProductsController(StoreContext context)
+    public ProductsController(IProductRepository repo)
     {
-        _context = context;
+        _repo = repo;
     }
 
     [HttpGet]
     public async Task<ActionResult<List<Product>>> GetProducts()
     {
-        return await _context.Products.ToListAsync();
+        return Ok(await _repo.GetProductsAsync());
     }
 
     [HttpGet("{id}")]
     public async Task<ActionResult<Product>> GetProduct(int id)
     {
-        return await _context.Products.FindAsync(id);
+        return await _repo.GetProductByIdAsync(id);
     }
 }
